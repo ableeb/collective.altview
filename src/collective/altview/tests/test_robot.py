330a73762653dce893d4599d1609ed0415c1cb00
Andy Leeb <ableeb@andersonleeb.com>
2014-06-13 13:10:00 -0400

    add src and contents

---
+++ src/collective/altview/tests/test_robot.py
@@ -0,0 +1,13 @@
+from  collective.altview.testing import COLLECTIVE_ALTVIEW_FUNCTIONAL_TESTING
+from plone.testing import layered
+import robotsuite
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite("robot_test.txt"),
+                layer=COLLECTIVE_ALTVIEW_FUNCTIONAL_TESTING)
+    ])
+    return suite
