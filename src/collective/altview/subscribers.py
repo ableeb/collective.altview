a4550490e06a3ca31c06bef51e390aad4999ae16
Andy Leeb <ableeb@andersonleeb.com>
2014-06-16 09:42:37 -0400

    PLONE-3252 - fix incorrect import

--- src/collective/altview/subscribers.py
+++ src/collective/altview/subscribers.py
@@ -4,7 +4,7 @@ from zope.lifecycleevent.interfaces import IObjectAddedEvent
 from Products.ATContentTypes.interfaces import IATNewsItem
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
-from uncsom.directory.settings import ISettings
+from collective.altview.settings import ISettings
 
 
 @grok.subscribe(IATNewsItem, IObjectAddedEvent)
