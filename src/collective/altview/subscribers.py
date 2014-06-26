3fa2e41e54715540430b4ea49b123d1da55758be
Andy Leeb <ableeb@andersonleeb.com>
2014-06-26 12:05:16 -0400

    PLONE-3337 - subscriber event responds to IObjectMovedEvent a super-class of IObjectAddedEvent and IObjectMovedEvent

--- src/collective/altview/subscribers.py
+++ src/collective/altview/subscribers.py
@@ -1,13 +1,14 @@
 from five import grok
 
-from zope.lifecycleevent.interfaces import IObjectAddedEvent
+#from zope.lifecycleevent.interfaces import IObjectAddedEvent
+from zope.lifecycleevent.interfaces import IObjectMovedEvent
 from Products.ATContentTypes.interfaces import IATNewsItem
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from collective.altview.settings import ISettings
 
 
-@grok.subscribe(IATNewsItem, IObjectAddedEvent)
+@grok.subscribe(IATNewsItem, IObjectMovedEvent)
 def set_news_item_layout(obj, event):
     registry = getUtility(IRegistry)
     settings = registry.forInterface(ISettings, check=False)
@@ -15,7 +16,3 @@ def set_news_item_layout(obj, event):
     if settings.path is not None and settings.views is not None:
         if settings.path in event.newParent.absolute_url_path():
             obj.setLayout(settings.views)
-
-            # list of tuples of availble views for content type
-            # available_layouts = obj.getAvailableLayouts()
-            # TODO: list registered views
